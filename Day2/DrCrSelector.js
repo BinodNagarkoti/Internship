1f1d3c882695e2bbf26b305b86a9c700cb8fe111
Binod Nagarkoti <Binod1365@gmail.com>
2019-05-26 19:02:12 +0545

    showing dr amount and cr amount differently

--- Day2/DrCrSelector.js
+++ Day2/DrCrSelector.js
@@ -1,22 +1,52 @@
-function getDr(){
-    var i=0;
-    for(i=0;i<4;i++){
-        console.log("sn"+1);
-        var san="san"+i;
-        var rf="rf"+i;
-       var particular= "particular"+i;
-       var dr="dr"+i;
-       var ta="ta"+i;
-        var getvalue=document.getElementById(dr);
-    if(getvalue.value!=0 ){
-       
-        var col1=document.getElementById(san).value; 
-        var col2=document.getElementById(rf).value; 
-        var col3=document.getElementById(particular).value; 
-        var col4=document.getElementById(dr).value;  
-        var col5=document.getElementById(cr).value;
-        var col6=document.getElementById(ta).value;  
-       console,console.log(col1,col2,col3,col4,col5,col6);
+// window.scope = {};
+var col1;
+var col2;
+var col3;
+var col4;
+var col5;
+var drJSON;
+function getDr() {
+    var i = 0;
+    var drjson = [];
+    for (i = 1; i <= 5; i++) {
+        var getvalue = document.getElementById("dr" + i);
+        if (getvalue.value != 0) {
+            col1 = document.getElementById("sn" + i).value;
+            col2 = document.getElementById("rf" + i).value;
+            col3 = document.getElementById("particular" + i).value;
+            col4 = document.getElementById("dr" + i).value;            // var col5=document.getElementById(cr).value;
+            col5 = document.getElementById("ta" + i).value;
+
+            drjson[i] = { sn: col1, rf: col2, particular: col3, dr: col4, ta: col5 };
+        }
+        
     }
-    }
+    // var drJSON= JSON.stringify(drjson);
+    localStorage.setItem('myStorage', JSON.stringify(drjson));
+    console.log(drJSON);
+    window.location.assign("DrFromJournal.html")
+    // self.location("DrFromJournal.html");
 }
+
+function getCr() {
+    var i = 0;
+    var crjson = [];
+    for (i = 1; i <= 5; i++) {
+        var getvalue = document.getElementById("cr" + i);
+        if (getvalue.value != 0) {
+            col1 = document.getElementById("sn" + i).value;
+            col2 = document.getElementById("rf" + i).value;
+            col3 = document.getElementById("particular" + i).value;
+            col4 = document.getElementById("cr" + i).value;
+            col5 = document.getElementById("ta" + i).value;
+            console, console.log(col1, col2, col3, col4, col5);
+            crjson[i] = { sn: col1, rf: col2, particular: col3, cr: col4, ta: col5 };
+
+        }
+    }
+    localStorage.setItem('myStorage2', JSON.stringify(crjson));
+    
+    window.location.assign("CrFromJournal.html");
+
+}
+
---
+++ Day2/Drgetter.js
@@ -0,0 +1,2 @@
+console.log(scope.display.sn);
+console.log(scope.cachedOption);
