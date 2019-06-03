f35b1c567098085cbbd88fced7b49ba7b56c8663
Binod Nagarkoti <Binod1365@gmail.com>
2019-06-03 08:45:37 +0545

    jango project

---
+++ week2/Django-FirstProject/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/BISHAL/Documents/OYD_internship/week2/Django-FirstProject/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
