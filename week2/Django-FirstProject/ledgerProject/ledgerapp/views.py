a797798172ddc608e42bb0113ec5866620672e0a
Binod Nagarkoti <Binod1365@gmail.com>
2019-06-04 16:45:44 +0545

    poll

--- week2/Django-FirstProject/ledgerProject/ledgerapp/views.py
+++ week2/Django-FirstProject/ledgerProject/ledgerapp/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 def ledger(request):
     return render(request,'ledger.html')
 def data(request):
-	dr = request.GET['dr1']	
-	return render(request, 'data.html',{'DebitAmount':float(dr) })
+	dr=''
+	cr=''
+	for i in range (1,5):
+		if int(request.GET['dr'+str(i)])!=0: 
+			dr=dr+','+(request.GET['dr'+str(i)])
+		if int(request.GET['cr'+str(i)])!=0:
+			cr=cr+','+(request.GET['cr'+str(i)])	
+	return render(request, 'data.html',{'DebitAmount':(dr),'CreditAmount':(cr) })
