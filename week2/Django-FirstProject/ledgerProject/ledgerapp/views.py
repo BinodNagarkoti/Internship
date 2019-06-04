3062e02f01859f06f330add9bbae14773c74ab67
BinodNagaekoti <Binod1365@gmail.com>
2019-06-04 14:06:30 +0545

    dr amount sent to data html

--- week2/Django-FirstProject/ledgerProject/ledgerapp/views.py
+++ week2/Django-FirstProject/ledgerProject/ledgerapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
-def ledger(response):
-    return render(response,'ledger.html')
+def ledger(request):
+    return render(request,'ledger.html')
+def data(request):
+    dr=(request.GET['dr1'])
+	return render(request, 'data.html',{'DebiAmount':float(dr)} )
