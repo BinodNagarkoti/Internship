c6ecd5dc3e75c22c9846112680899ee5282f361b
Binod Nagarkoti <Binod1365@gmail.com>
2019-06-05 05:05:24 +0545

    templalete inheritence

---
+++ week2/Django-FirstProject/lessionTemplateInheritance-master/bTemplate/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+def bTemplate(request):
+    return render(request,'btemplate.html')
