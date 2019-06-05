ee2ed85ea14c0e7323460ecdd77af0f470394d44
Binod Nagarkoti <Binod1365@gmail.com>
2019-06-05 17:39:36 +0545

    fix templete Inheritence

--- week2/Django-FirstProject/lessionTemplateInheritance-master/base/views.py
+++ week2/Django-FirstProject/lessionTemplateInheritance-master/base/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-def base(request):
-    return render(request, 'base.html')
-def submit(request):
-	value=request.GET['abc']
+def radio(request):
+    return render(request, 'radio.html')
+def showData(request):
+    values=request.GET['abc']
     value2=request.GET['efg']
     value3=request.GET['hij']
-    if value ==='a':
-        return render(request, 'atemplate.html',{'values1':(value),'value2':(value2),'value3':(value3)})
-    elif value ==='b':
-        return render(request, 'btemplate.html',{'values1':(value),'value2':(value2),'value3':(value3)})   
+    if values =='a':
+        return render(request, 'atemplate.html',{'values1':(values),'values2':(value2),'values3':(value3)})
+    elif values =='b':
+        return render(request, 'btemplate.html',{'values1':(values),'values2':(value2),'values3':(value3)})   
     else:
-        return render(request, 'ctemplate.html',{'values1':(value),'value2':(value2),'value3':(value3)})
+        return render(request, 'ctemplate.html',{'values1':(values),'values2':(value2),'values3':(value3)})
     			
 	
 
