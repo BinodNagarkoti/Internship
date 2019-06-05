c5b0115f389ea33af77c0a9030fdc7211959d3bf
BinodNagaekoti <Binod1365@gmail.com>
2019-06-05 15:05:51 +0545

    lession templete inheritence edited

--- week2/Django-FirstProject/lessionTemplateInheritance-master/base/views.py
+++ week2/Django-FirstProject/lessionTemplateInheritance-master/base/views.py
@@ -3,4 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     return render(request, 'base.html')
+def submit(request):
+	value=request.GET['abc']
+    value2=request.GET['efg']
+    value3=request.GET['hij']
+    if value ==='a':
+        return render(request, 'atemplate.html',{'values1':(value),'value2':(value2),'value3':(value3)})
+    elif value ==='b':
+        return render(request, 'btemplate.html',{'values1':(value),'value2':(value2),'value3':(value3)})   
+    else:
+        return render(request, 'ctemplate.html',{'values1':(value),'value2':(value2),'value3':(value3)})
+    			
+	
+
 
