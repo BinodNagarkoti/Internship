f01a180e5bdaa683f01c7eb5e81724de00a23089
Binod Nagarkoti <Binod1365@gmail.com>
2019-06-07 06:25:23 +0545

    form crispy forms

--- week2/Django-FirstProject/MyForm/Form/settings.py
+++ week2/Django-FirstProject/MyForm/Form/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'baseForm.apps.BaseformConfig',
+    ''
     'LoginForm.apps.LoginformConfig',
     'RegisterForm.apps.RegisterformConfig',
+    'crispy_forms'
 
 ]
 
@@ -120,5 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+
+]
